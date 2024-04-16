9647d269b498d6174045cf73cabce47d497e4ebb
chris <iradukundachristian202@gmail.com>
2024-04-16 15:03:47 +0200

    updates in index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import router from './router/routes';
-import dotenv from 'dotenv'
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router/routes";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
+const port = process.env.PORT;
 
-app.use(cors({
-    origin: 'http://localhost:5000',
-    credentials: true
-
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.use(cookieParser());
@@ -24,22 +26,19 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000/')
-})
+server.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
 
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL as string);
+mongoose.connection.on("error", (error: Error) => {
+  console.log(error);
+});
 
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGO_URL);
-// mongoose.connection.on('error', (error: Error) => {
-//     console.log(error);
-// });
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((result) => console.log("database connected"))
+  .catch((err) => console.log(err));
 
-
-mongoose.connect(process.env.MONGO_URL as string, {})
-    .then(result => console.log('database connected'))
-    .catch(err => console.log(err))
-
-
-
-app.use('/api', router)
+app.use("/api/V1", router);
