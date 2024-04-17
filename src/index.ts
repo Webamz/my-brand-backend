9ce20c91d614c1556d1ff9a2383e6a7b5d752f4b
chris <iradukundachristian202@gmail.com>
2024-04-17 16:03:25 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.use(cookieParser());
-app.use(.json());
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
