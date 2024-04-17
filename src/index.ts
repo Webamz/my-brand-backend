dd6d830d89025685b8cbdfc73ab94d3f1833f6dc
chris <iradukundachristian202@gmail.com>
2024-04-17 13:23:45 +0200

    reverting the change

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
@@ -21,7 +22,7 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.use(cookieParser());
-app.use(express.json());
+app.use(.json());
 
 const server = http.createServer(app);
 
